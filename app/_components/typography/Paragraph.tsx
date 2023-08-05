e51340adbd64d1aa7dd2e319175c7c1867b30231
Swiichy <adlpromail@gmail.com>
2023-08-05 15:13:53 +0200

    refact: remove hex color by tailwind theme color

--- app/_components/typography/Paragraph.tsx
+++ app/_components/typography/Paragraph.tsx
@@ -6,7 +6,7 @@ type Props = {
 export default function Paragraph({ text, className }: Props) {
   return (
     <p
-      className={`font-medium text-base leading-6 text-[#D9D9D9] md:text-lg md:leading-7 ${className}`}
+      className={`font-medium text-base leading-6 text-primary-grey md:text-lg md:leading-7 ${className}`}
     >
       {text}
     </p>
