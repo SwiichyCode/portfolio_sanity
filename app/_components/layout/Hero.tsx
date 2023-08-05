fe40f921d9ca44a423ba5e9bd869c1fa6af0a704
Swiichy <adlpromail@gmail.com>
2023-08-05 10:54:54 +0200

    fix: escaped error

--- app/_components/layout/Hero.tsx
+++ app/_components/layout/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section className="flex flex-col justify-center items-center text-center color-white gap-6 px-4 md:items-start md:text-left md:gap-14 md:px-8 md:mb-14 lg:max-w-section lg:w-full">
       <h1 className=" font-bold text-[40px] leading-10 tracking-[-1.13636px] md:flex md:flex-col md:gap-2">
-        Enchanté ! Je m'apelle{" "}
+        Enchanté ! Je m&apos;apelle{" "}
         <span className="underline decoration-primary-green md:text-[72px] md:leading-[72px] md:tracking-[-2.04545px] lg:text-[88px] lg:leading-[88px]">
           Dylan Jansana.
         </span>
