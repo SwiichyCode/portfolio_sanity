d5a5880ec151f7f5196474740b4bfe0fa12c9ba8
Swiichy <adlpromail@gmail.com>
2023-08-05 15:22:19 +0200

    .

--- app/_components/common/Button.tsx
+++ app/_components/common/Button.tsx
@@ -8,7 +8,7 @@ interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 
 export default function Button({ text, as, href, ...props }: Props) {
   const className =
-    "w-fit font-bold text-base leading-6 tracking-[2.28571px] uppercase border-b-2 border-solid border-[#4EE1A0] pb-2 ease-in-out duration-200 hover:text-primary-green";
+    "w-fit font-bold text-base leading-6 tracking-[2.28571px] uppercase border-b-2 border-solid border-primary-green pb-2 ease-in-out duration-200 hover:text-primary-green";
 
   return as === "button" ? (
     <button className={className} {...props}>
