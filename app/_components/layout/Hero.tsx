39d830c5c3929aae81a10690c1dc878ddb3374cc
Swiichy <adlpromail@gmail.com>
2023-08-05 10:51:49 +0200

    feat: add metadata

--- app/_components/layout/Hero.tsx
+++ app/_components/layout/Hero.tsx
@@ -6,19 +6,18 @@ export default function Hero() {
   return (
     <section className="flex flex-col justify-center items-center text-center color-white gap-6 px-4 md:items-start md:text-left md:gap-14 md:px-8 md:mb-14 lg:max-w-section lg:w-full">
       <h1 className=" font-bold text-[40px] leading-10 tracking-[-1.13636px] md:flex md:flex-col md:gap-2">
-        Nice to meet you! I’m{" "}
+        Enchanté ! Je m'apelle{" "}
         <span className="underline decoration-primary-green md:text-[72px] md:leading-[72px] md:tracking-[-2.04545px] lg:text-[88px] lg:leading-[88px]">
           Dylan Jansana.
         </span>
       </h1>
       <div className="flex flex-col items-center gap-6 md:items-start">
         <Paragraphe
-          text="Based in France, I’m a front-end developer passionate about
-          building accessible web apps that users love."
+          text="Je réside en France, et je suis un développeur front-end passionné par la création d'applications web attrayantes et conviviales."
           className="w-full max-w-[445px]"
         />
         <Button
-          text="contact me"
+          text="contacte moi"
           as="button"
           onClick={() => console.log("test")}
         />
--- app/_components/layout/Projects.tsx
+++ app/_components/layout/Projects.tsx
@@ -13,8 +13,8 @@ export default function Projects({ projects }: Props) {
   return (
     <section className="flex flex-col justify-center items-center px-4 md:mb-24 md:px-8 lg:w-full lg:max-w-section">
       <header className="w-full flex justify-between items-center mb-10 md:mb-14">
-        <Title text="Projects" as="h2" />
-        <Button text="contact me" as="button" />
+        <Title text="Projets" as="h2" />
+        <Button text="contacte moi" as="button" />
       </header>
       <AnimatedList className="w-full flex flex-wrap justify-center min-[1190px]:justify-between items-center gap-10  md:gap-x-7 md:gap-y-16">
         {projects.map((project, index) => (
